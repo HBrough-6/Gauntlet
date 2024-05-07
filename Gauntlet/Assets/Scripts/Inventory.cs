81138cb754af1cd45a976ff1f9c9561faae6998b
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2024-05-06 20:44:10 -0700

    started inventory

---
+++ Gauntlet/Assets/Scripts/Inventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private int numKeys = 0;
+    private int numPotions = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(Item item)
+    {
+
+    }
+
+    public void activatePotion()
+    {
+
+    }
+
+    public void RemoveItem(Item item)
+    {
+
+    }
+}
