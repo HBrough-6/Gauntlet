9061177db4bf30c281bf73a7c08393343dbe79c5
HBrough-6 <130100349+HBrough-6@users.noreply.github.com>
2024-05-13 07:07:13 -0700

    ?

---
+++ Gauntlet/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    private bool strengthUpgraded;
+    private bool shotPowerUpgraded;
+    private bool shotSpeedUpgraded;
+    private bool magicVMonsterUpgraded;
+    private bool magicVGeneratorUpgraded;
+    private bool potionShotVMonsterUpgraded;
+    private bool potionShotVGeneratorUpgraded;
+    private bool meleeVGeneratorsUpgraded;
+    private bool runningSpeedUpgraded;
+
+
+    [SerializeField] private StatConfig stats;
+
+
+}
